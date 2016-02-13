88d9eb1700aa8fbf74c9bd35c0ba2a3ffcc4802b
Kessy Similien <kessy.similien@weplaytoday.com>
2016-02-12 21:58:40 -0500

    upload images to s3 and change contact us img

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ApplicationMailer
   default from: 'info@spdash.com',
-          to: 'simkessy@gmail.com',
+          to: 'info@spdahs.com',
           subject: "SpDash Contact Message"
 
   def send_message contact
